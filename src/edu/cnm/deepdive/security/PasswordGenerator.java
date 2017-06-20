05edef7350c73cb71ea28f52588775e34ed09bc3
yolanda philgreen <yolanda.philgreen@gmail.com>
2017-06-20 12:03:42 -0600

    first set up of new project to password generator

---
+++ src/edu/cnm/deepdive/security/PasswordGenerator.java
@@ -0,0 +1,25 @@
+/*
+ * PasswordGenerator.java
+ * 
+ */
+
+package edu.cnm.deepdive.security;
+
+/**
+ * Implementation of a simple password generator.
+ * 
+ * @author Yolanda Philgreen
+ * @version 1.0
+ */
+public class PasswordGenerator {
+/**
+ * Test rig for generating passwords.
+ * 
+ * @param args Command-line parameters for password generation options.
+ */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
