050d286f556f5307199a924a21d13e93f4f4b38d
yolanda philgreen <yolanda.philgreen@gmail.com>
2017-06-29 16:13:19 -0600

    added To do list in comments field

--- src/edu/cnm/deepdive/security/PasswordGenerator.java
+++ src/edu/cnm/deepdive/security/PasswordGenerator.java
@@ -13,6 +13,8 @@ import java.util.Random;
  * @author Yolanda Philgreen
  * @version 1.0
  */
+
+
 public class PasswordGenerator {
   
   private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -32,11 +34,11 @@ public class PasswordGenerator {
   private boolean punctuationInclude = true;
   private boolean ambiguousExclude = true;
   private String delimiter = "";
+  //TODO warnings that will be added are options in conflict password done and the delimeter and word list.
+  //TODO warnings extreme value, high length
+ //TODO set length min and max to length option value set character set fields from options same with  ambiguous. 
+  //TODO do we set a warning on things line an unknown option we can throw a warning and then let them do it.
   
-  
-  /**
-   * 
-   */
  public PasswordGenerator() {
  }
    
